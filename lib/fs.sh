a8af4629b235e00a8daac520cce4fb87bbd17163
Les Hazlewood <121180+lhazlewood@users.noreply.github.com>
2018-06-15 22:43:41 -0400

    initial ssh config work

---
+++ lib/fs.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+
+command -v strap::lib::import >/dev/null || { echo "strap::lib::import is not available" >&2; exit 1; }
+strap::lib::import logging || . logging.sh
+
+set -a
+
+strap::fs::chmod() {
+
+  local chmod="${1:-}" && [[ -z "$chmod" ]] && strap::abort 'strap::fs::chmod: $1 must be the chmod mode'
+  local target="${2:-}" && [[ -z "$target" ]] && strap::abort 'strap::fs::chmod: $2 must be the chmod target'
+  local name="${3:-}" && [[ -z "$name" ]] && name="$target"
+
+  strap::running "Ensuring chmod $chmod on $name"
+  chmod ${chmod} "$target"
+  strap::ok
+}
+
+strap::fs::dir::ensure() {
+
+  local dir="${1:-}"
+  local chmod="${2:-}"
+  local name="${3:-}" && [[ -z "$name" ]] && name="$dir"
+
+  strap::running "Checking directory: $name"
+  if [[ ! -d "$dir" ]]; then
+    [[ -f "$dir" ]] && strap::error "$dir is an existing file and cannot be made a directory" && return 1
+    strap::action "Creating directory: $name"
+    mkdir -p "$dir"
+  fi
+  strap::ok
+
+  [[ -n "$chmod" ]] && strap::fs::chmod "$chmod" "$dir" "$name"
+}
+
+strap::fs::file::ensure() {
+
+  local file="${1:-}"
+  local chmod="${2:-}"
+  local name="${3:-}" && [[ -z "$name" ]] && name="$file"
+
+  strap::running "Checking file: $name"
+  if [[ ! -f "$file" ]]; then
+    [[ -d "$file" ]] && strap::error "$file is an existing directory and cannot be made a file" && return 1
+    strap::action "Creating file: $name"
+    touch "$file"
+  fi
+  strap::ok
+
+  [[ -n "$chmod" ]] && strap::fs::chmod "$chmod" "$file" "$name"
+}
+
+set +a
--- lib/os.sh
+++ lib/os.sh
@@ -46,6 +46,29 @@ _EOF_
   fi
 }
 
+# From https://stackoverflow.com/a/48487783/407170
+strap::semver::compare() {
+
+  local a="${1:-}" op="${2:-}" b="${3:-}" al="${a##*.}" bl="${b##*.}"
+  while [[ $al =~ ^[[:digit:]] ]]; do al=${al:1}; done
+  while [[ $bl =~ ^[[:digit:]] ]]; do bl=${bl:1}; done
+  local ai=${a%$al} bi=${b%$bl}
+
+  local ap=${ai//[[:digit:]]} bp=${bi//[[:digit:]]}
+  ap=${ap//./.0} bp=${bp//./.0}
+
+  local w=1 fmt=$a.$b x IFS=.
+  for x in $fmt; do [ ${#x} -gt $w ] && w=${#x}; done
+  fmt=${*//[^.]}; fmt=${fmt//./%${w}s}
+  printf -v a $fmt $ai$bp; printf -v a "%s-%${w}s" $a $al
+  printf -v b $fmt $bi$ap; printf -v b "%s-%${w}s" $b $bl
+
+  case $op in
+    '<='|'>=' ) [ "$a" ${op:0:1} "$b" ] || [ "$a" = "$b" ] ;;
+    * )         [ "$a" $op "$b" ] ;;
+  esac
+}
+
 strap::os::version() {
   local flags="${1:-}"
   local version
